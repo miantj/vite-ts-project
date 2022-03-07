c59a8110b11e74ac6ff31844f83a2bdc5c9303b4
Lyle <934854233@qq.com>
2022-03-07 18:35:46 +0800

    .

--- src/config/iconfont/index.ts
+++ src/config/iconfont/index.ts
@@ -1,13 +1,6 @@
-import iconifyIconOffline from "./src/iconifyIconOffline";
-import iconifyIconOnline from "./src/iconifyIconOnline";
-import fontIcon from "./src/iconfont";
+import fontIcon from './src/iconfont'
+export const FontIcon = fontIcon
 
-export const IconifyIconOffline = iconifyIconOffline;
-export const IconifyIconOnline = iconifyIconOnline;
-export const FontIcon = fontIcon;
-
-export default { 
-  IconifyIconOffline,
-  IconifyIconOnline,
-  FontIcon
-};
+export default {
+    FontIcon,
+}
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import router from './router'
 import { setupStore } from '@/store'
 // 导入公共样式
 import './style/index.scss'
-
+import * as ElIconModules from '@element-plus/icons-vue'
 const app = createApp(App)
 
+// 全局注册图标库
+for (const iconName in ElIconModules) {
+    if (Reflect.has(ElIconModules, iconName)) {
+        const item = ElIconModules[iconName]
+        app.component(iconName, item)
+    }
+}
+
 app.use(router)
-// setupStore(app)
+// setupStore(app) 
 await router.isReady()
 app.mount('#app')
