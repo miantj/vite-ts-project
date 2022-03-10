2643a5b9a56ad72a400fb07f3c9d52a83d38677c
Lyle <934854233@qq.com>
2022-03-11 00:21:25 +0800

    添加全屏插件

--- src/layout/components/navbar/index.vue
+++ src/layout/components/navbar/index.vue
@@ -2,10 +2,29 @@
     <div class="navbar">
         <!-- <Hamburger class="hamburger-container" /> -->
 
-        <div class="hamburger-container" @click="open">
+        <div class="hamburger-container" :title="isCollapse ? '点击展开' : '点击折叠'" @click="open">
             <IconFont style="font-size: 20px" :icon="isCollapse ? 'icon-indent' : 'icon-outdent'" />
         </div>
-        <Breadcrumb />
+        <Breadcrumb class="breadcrumb-container" />
+        <div class="vertical-header-right">
+            <Screenfull />
+
+            <!-- 退出登陆 -->
+            <el-dropdown trigger="click">
+                <span class="el-dropdown-link">
+                    <img src="https://avatars.githubusercontent.com/u/29756128?s=40&v=4" />
+                    <p>admin</p>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu class="logout">
+                        <el-dropdown-item @click="logout">
+                            <IconFont style="font-size: 20px" icon="icon-logout" />
+                            退出系统
+                        </el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
     </div>
 </template>
 
@@ -13,6 +32,7 @@
 import { useLayoutStoreHook } from '@/store/modules/layout'
 import { useNav } from '../../hook/nav'
 import Breadcrumb from './breadcrumb.vue'
+import Screenfull from './screenfull.vue'
 
 const { isCollapse } = useNav()
 
@@ -20,6 +40,7 @@ function open() {
     useLayoutStoreHook().toggleSideBar(!useLayoutStoreHook().sidebar.opened)
     console.warn(useLayoutStoreHook().sidebar.opened)
 }
+function logout() {}
 </script>
 
 <style lang="scss" scoped>
@@ -40,6 +61,29 @@ function open() {
         transition: background 0.3s;
         -webkit-tap-highlight-color: transparent;
         padding: 0 15px;
+    }
+    .breadcrumb-container {
+        font-size: 14px;
+        line-height: 48px;
+        float: left;
+    }
+    .vertical-header-right {
+        display: flex;
+        min-width: 280px;
+        height: 48px;
+        align-items: center;
+        color: #000000d9;
+        justify-content: flex-end;
+    }
+
+    .logout {
+        max-width: 120px;
+
+        ::v-deep(.el-dropdown-menu__item) {
+            min-width: 100%;
+            display: inline-flex;
+            flex-wrap: wrap;
+        }
     }
 }
 </style>
---
+++ src/layout/components/navbar/screenfull.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="screen-full" @click="toggle">
+        <IconFont
+            style="font-size: 20px"
+            :title="isFullscreen ? '退出全屏' : '全屏'"
+            :icon="isFullscreen ? 'icon-fullscreen-exit' : 'icon-fullscreen'"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core'
+const { isFullscreen, toggle } = useFullscreen()
+</script>
+
+<style lang="scss" scoped>
+.screen-full {
+    width: 36px;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    justify-content: space-around;
+
+    &:hover {
+        background: #f6f6f6;
+    }
+}
+</style>
