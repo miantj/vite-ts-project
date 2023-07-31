f8c3b3a33373ea8f9e258b780346eda63cfa5a2b
mingtj <934854233@qq.com>
2023-07-31 22:19:02 +0800

    优化样式

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default defineComponent({
     },
     computed: {
         currentLocale() {
-            return zhCn
+            // return zhCn
         },
     },
     mounted() {},
--- src/views/home.vue
+++ src/views/home.vue
@@ -88,54 +88,95 @@ onMounted(async () => {
                 <el-col :span="20">
                     <div class="grid">
                         <div class="active"></div>
-                        <div class="grid-item grid-start" style="background: #ffff33">起点</div>
+                        <div class="grid-item grid-start" style="background: #ffff33">
+                            <p></p>
+                            <p class="text">起点GO</p>
+                        </div>
                         <div class="grid-item" style="background: #529b2e">
-                            1 小明常用污言秽语对其他学生进行攻击，从而产生矛盾 蹲起3个
+                            <i class="index">1</i>
+                            <div>小明常用污言秽语对其他学生进行攻击，从而产生矛盾 蹲起3个</div>
                         </div>
                         <div class="grid-item" style="background: #337ecc">
-                            2 开展校园欺凌治理为主题的专题教育，开展品德。心理健康和安全教育
+                            <i class="index">2</i>
+                            <div>开展校园欺凌治理为主题的专题教育，开展品德。心理健康和安全教育</div>
                         </div>
                         <div class="grid-item" style="background: #f3d19e">
-                            3 给李明取侮辱性绰号侮辱他人人格 原地转1圈
+                            <i class="index">3</i>
+                            <div>给李明取侮辱性绰号侮辱他人人格 原地转1圈</div>
                         </div>
                         <div class="grid-item" style="background: #f89898">
-                            4 我国刑法对犯罪年龄的界定是（） A已满16周岁应当负刑事责任
-                            B已满14周岁不满16周岁故意伤害不负刑责 C已满16周岁不负刑事责任
+                            <i class="index">4</i>
+                            <div>
+                                我国刑法对犯罪年龄的界定是（） A已满16周岁应当负刑事责任
+                                B已满14周岁不满16周岁故意伤害不负刑责 C已满16周岁不负刑事责任
+                            </div>
                         </div>
                         <div class="grid-item" style="background: #a0cfff">
-                            5 看到同学被欺负了，高年级张明及时制止，并告知老师 前进一格
+                            <i class="index">5</i>
+                            <div>看到同学被欺负了，高年级张明及时制止，并告知老师 前进一格</div>
                         </div>
-                        <div class="grid-item" style="background: #ab9bb8">6此插画是校园欺凌吗？</div>
+                        <div class="grid-item" style="background: #ab9bb8">
+                            <i class="index">6</i>
+                            <div>此插画是校园欺凌吗？</div>
+                        </div>
                         <div class="grid-item" style="background: #fab6b6">
-                            7 王明借助身体的优势打骂比较弱小的同学 原地转1圈
+                            <i class="index">7</i>
+                            <div>王明借助身体的优势打骂比较弱小的同学 原地转1圈</div>
                         </div>
-                        <div class="grid-item" style="background: #f89898">8 后退2步</div>
-                        <div class="grid-item" style="background: #f3d19e">9 再接再厉 在投一次</div>
                         <div class="grid-item" style="background: #f89898">
-                            10 同学小张向低年级学生索要钱物，不给就软硬兼施，威逼利诱
+                            <i class="index">8</i>
+                            <div>后退2步</div>
+                        </div>
+                        <div class="grid-item" style="background: #f3d19e">
+                            <i class="index">9</i>
+                            <div>再接再厉 在投一次</div>
+                        </div>
+                        <div class="grid-item" style="background: #f89898">
+                            <i class="index">10</i>
+                            <div>同学小张向低年级学生索要钱物，不给就软硬兼施，威逼利诱</div>
                         </div>
                         <div class="grid-item" style="background: #a0cfff">
-                            11 遭遇校园暴力，应在学校、警方或家长的帮助下，制止暴力，决不能逆来顺受或以暴制暴
+                            <i class="index">11</i>
+                            <div>遭遇校园暴力，应在学校、警方或家长的帮助下，制止暴力，决不能逆来顺受或以暴制暴</div>
                         </div>
                         <div class="grid-item" style="background: #faecd8">
-                            12 以下哪个关于欺凌的观点是正确的？ A.孩子只是在互相开玩笑 B.孩子收到欺凌告诉他要反击回去
-                            C.孩子告诉家长被同学欺负，家长责怪其胆小、懦弱等
-                            D.欺凌者。被欺凌者都是受欺凌行为影响的孩子，家长需要帮助孩子找办法应对和处理
+                            <i class="index">12</i>
+                            <div>
+                                以下哪个关于欺凌的观点是正确的？ A.孩子只是在互相开玩笑 B.孩子收到欺凌告诉他要反击回去
+                                C.孩子告诉家长被同学欺负，家长责怪其胆小、懦弱等
+                                D.欺凌者。被欺凌者都是受欺凌行为影响的孩子，家长需要帮助孩子找办法应对和处理
+                            </div>
                         </div>
                         <div class="grid-item" style="background: #c45656">
-                            13 张鹏不断地用语言、行为等给其他同学造成精神或心理上的压力，使其出现不良表现
+                            <i class="index">13</i>
+                            <div>张鹏不断地用语言、行为等给其他同学造成精神或心理上的压力，使其出现不良表现</div>
                         </div>
-                        <div class="grid-item" style="background: #a0cfff">14 遇到校园欺凌了，你该怎么处理？</div>
+                        <div class="grid-item" style="background: #a0cfff">
+                            <i class="index">14</i>
+                            <div>遇到校园欺凌了，你该怎么处理？</div>
+                        </div>
                         <div class="grid-item" style="background: #9381a9">
-                            15 发现同学（）可能是校园欺凌的信号 A身体表面无故出现瘀伤、抓伤等 B不同某同学玩
-                            C不愿借东西给某同学
+                            <i class="index">15</i>
+                            <div>
+                                发现同学（）可能是校园欺凌的信号 A身体表面无故出现瘀伤、抓伤等 B不同某同学玩
+                                C不愿借东西给某同学
+                            </div>
                         </div>
-                        <div class="grid-item" style="background: #f3d19e">16 说一句反对校园欺凌的标语</div>
-                        <div class="grid-item" style="background: #79bbff">17校园欺凌的危害</div>
+                        <div class="grid-item" style="background: #f3d19e">
+                            <i class="index">16</i>
+                            <div>说一句反对校园欺凌的标语</div>
+                        </div>
                         <div class="grid-item" style="background: #79bbff">
-                            18 校园欺凌下面哪个是（） A不跟同学说话 B网上传播谣言，人身攻击 不借物品
+                            <i class="index">17</i>
+                            <div>校园欺凌的危害</div>
                         </div>
-                        <div class="grid-item grid-end" style="background: #b43d63">终端</div>
+                        <div class="grid-item" style="background: #79bbff">
+                            <i class="index">18</i>
+                            <div>校园欺凌下面哪个是（） A不跟同学说话 B网上传播谣言，人身攻击 不借物品</div>
+                        </div>
+                        <div class="grid-item grid-end" style="background: #b43d63">
+                            <p class="text">终点</p>
+                        </div>
                     </div>
                 </el-col>
                 <el-col :span="4">
@@ -150,6 +191,8 @@ onMounted(async () => {
     width: 1024;
     background: url('@/assets/img/bg1.jpg') no-repeat;
     background-size: 100% 100%;
+    font-family: 'Microsoft Yahei';
+    font-weight: 500;
 }
 :deep(.grid) {
     position: relative;
@@ -160,8 +203,8 @@ onMounted(async () => {
 
     .active {
         position: absolute;
-        top: 0;
-        left: 0;
+        top: -5px;
+        left: -10px;
         background: url(/src/assets/img/ren.png) no-repeat;
         background-size: 100% 100%;
         width: 90px;
@@ -174,11 +217,35 @@ onMounted(async () => {
 
     .grid-item {
         position: absolute;
-        padding: 10px;
+        padding: 26px 10px;
         width: 140px;
         height: 140px;
         border: 3px solid #111;
-        color: #fff;
+
+        .index {
+            position: absolute;
+            top: 4px;
+            right: 4px;
+            width: 22px;
+            height: 22px;
+            line-height: 22px;
+            text-align: center;
+            background: #b43d63;
+            border-radius: 50%;
+            color: #fff;
+            font-style: normal;
+            font-size: 14px;
+        }
+        div {
+            color: #fff;
+            font-size: 18px;
+            text-align: center;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 4;
+            -webkit-box-orient: vertical;
+        }
     }
     .grid-start,
     .grid-end {
@@ -186,8 +253,26 @@ onMounted(async () => {
         height: 180px;
         border-radius: 5px;
     }
-    .grid-start {
+    .grid-start,
+    .grid-end {
         background: #b43d63;
+        .text {
+            font-family: 'DynaPuff', cursive;
+            text-align: center;
+            width: 150px;
+            font-size: 40px;
+            position: absolute;
+            top: 80%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-shadow: 0 0px orange, 1px 1px black;
+            -webkit-text-fill-color: transparent;
+        }
+    }
+    .grid-end {
+        .text {
+            text-shadow: 0 0px #ffff33, 1px 1px black;
+        }
     }
 }
 </style>
